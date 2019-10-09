ee649eed952ccb1b5b1fcf1f0c64d43fe5783e15
evgeniy gazetdinov <zplacesound@gmail.com>
2019-10-09 12:27:37 +0300

    Create admin3.py

---
+++ admin3.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+from django.contrib import admin
+from apps.slider.models import SiteMainPageSlider
+from django import forms
+from django.contrib import messages
+import pytils, os, zipfile
+
+
+class SliderAdmin(admin.ModelAdmin):
+
+    def save_model(self,request,obj,form,change):
+        if obj.slider_type == 0:
+            if obj.image == "":
+                messages.set_level(request, messages.ERROR)
+                messages.error(request,'нет картинки для компьютера')
+            if obj.image_tablet == None:
+                messages.set_level(request, messages.ERROR)
+                messages.error(request,'нет картинки для планшета')
+            if obj.image_mobile == None:
+                messages.set_level(request, messages.ERROR)
+                messages.error(request,'нет картинки для смартфона')
+            else:
+                obj.save()
+            
+#            messages.error(request,'не заполнены все поля')
+        if obj.slider_type == 1:
+            if obj.html5_zip is not None:
+                obj.save()
+        
+            
+
+
+    list_display = ('ordering', 'title')
+admin.site.register(SiteMainPageSlider, SliderAdmin)
+
+
