4eee65f9d56793c1c403f708fe81f8fa776d431c
evgeniy gazetdinov <zplacesound@gmail.com>
2019-11-04 20:08:40 +0300

    Update serializer.py

--- serializer.py
+++ serializer.py
@@ -1,28 +1,17 @@
-# coding=utf-8
+# -- coding: utf-8 --
 import pytils
 from apps.tv.models import TVProgramItem,TVProgram
 from rest_framework import serializers
 from apps.programms.models import ProgramItem
-
-class TVSERIALIZER(serializers.ModelSerializer):
-    title = serializers.ReadOnlyField(source='program.title')
-    class Meta:
-        model = TVProgramItem
-        fields = ('program')
-
-
-class TVSheduleSerializer(serializers.ModelSerializer):
-    days = serializers.SerializerMethodField(method_name ="")
-
-
+import re
+from django.http import HttpResponse
 
 
 
 
 class TVProgrammsSerializer(serializers.ModelSerializer):
     time = serializers.SerializerMethodField(method_name='tv_time')
-    date_start = serializers.SerializerMethodField(method_name='date_start')
-    date_end = serializers.SerializerMethodField(method_name='')
+    date = serializers.SerializerMethodField(method_name='start_date')
     url = serializers.ReadOnlyField(source='get_absolute_url')
     title = serializers.ReadOnlyField(source='program.title')
     rating = serializers.ReadOnlyField(source='program.rating')
@@ -55,19 +44,91 @@ class TVProgrammsSerializer(serializers.ModelSerializer):
         )
 
 
+
 class AdminTVProgrammSerializer(TVProgrammsSerializer):
     def start_date(self, obj):
         return obj.date_start.strftime('%d.%m.%Y')
 
-class SuperSerializer(serializers.ModelSerializer):
-    watch_now = serializers.SerializerMethodField('programs_on_day')
 
+class TVProgramsSchedulerSerializer(TVProgrammsSerializer):
+    image = serializers.SerializerMethodField(method_name='get_image_relative_url')
+    url = serializers.SerializerMethodField(method_name='get_program_url')
+    add_to_schedule = serializers.SerializerMethodField(method_name='button_schedule')
+
+    def time_format(self,obj):
+        return  obj.date_start.strftime("%Y%m%dT%H%M%S")
+
+
+
+
+    def button_schedule(self,obj):
+        if obj.program.can_schedule == True:
+            #radical
+            import sys
+            reload(sys)
+            sys.setdefaultencoding('utf8')
+            #add to each word % for sticky in url
+            button_titles = "Посмотреть%<<{title}>>на%Канале%Disney%по%телевизору!".format(title = "%20".join(str(obj.program.title.decode()).split()))
+            return "{host}/tv/do-schedule/{id}?date_start={date_start}&{date_end}&tz={tz}&title={title}".format(host=self.context['request'],
+                    id=obj.program.id,tz = self.context['timezone'], date_start = self.time_format(obj),
+                    date_end = obj.date_end, title = button_titles)
+        else:
+            return ''
+
+
+    def get_program_url(self,obj):
+        if obj:
+            pattern_protocol = '\w+://\w+.\w+'
+            without_protocol = '/\w+\w+/'
+            name = str(obj.get_absolute_url())
+            if (re.match(without_protocol,name) is not None):
+                if name is not None:
+                    return str(self.context['request'])+str(obj.get_absolute_url())
+            if (re.match(pattern_protocol,name) is not None):
+                if name is not None:
+                        return str(obj.get_absolute_url())
+        else:
+            return ''
+
+
+
+    def get_image_relative_url(self, obj):
+        if obj and obj.program and obj.program.image:
+            pattern_protocol = '\w+://\w+.\w+'
+            without_protocol = '/\w+\w+/'
+            name = obj.program.image.url
+            if (re.match(without_protocol,name) is not None):
+                if name is not None:
+                    return str(self.context['request'])+str(obj.program.image.url)
+            if (re.match(pattern_protocol,name) is not None):
+                if name is not None:
+                        return obj.program.image.url
+        else:
+            return ''
+
+
+    class Meta:
+        model = TVProgramItem
+        fields = (
+            'id',
+            'time',
+            'date',
+            'rating',
+            'title',
+            'image',
+            'url',
+            'add_to_schedule'
+        )
+
+
+
+class ScheduleApiSerializer(serializers.ModelSerializer):
+    watch_now = serializers.SerializerMethodField('programs_on_day')
     def programs_on_day(self,obj):
         qs = TVProgramItem.objects.filter(programs = obj.pk).order_by('date_end')
-        serializer = TVProgrammsSerializer(instance=qs, many=True)
+        serializer = TVProgramsSchedulerSerializer(instance=qs, many=True,context =self.context)
         return serializer.data
-    
-    
+
     class Meta:
         model = TVProgram
         fields = ('day','watch_now')
